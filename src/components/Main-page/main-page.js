a82c0c95f1198d25274e26de78f8848e100fc749
6677811 <79306475+6677811@users.noreply.github.com>
2021-08-01 23:23:55 +0300

    feat: add ready-made solution

---
+++ src/components/Main-page/main-page.js
@@ -0,0 +1,128 @@
+import React from 'react';
+
+const MainPage = () => (
+    <div className={'d-flex justify-content-center flex-row flex-wrap'}>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+        <div className="card border-primary m-3" style={{ maxWidth: '20rem' }}>
+            <div className="card-header">Header</div>
+            <div className="card-body">
+                <h4 className="card-title">Primary card title</h4>
+                <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                </p>
+            </div>
+        </div>
+    </div>
+);
+
+export default MainPage;
